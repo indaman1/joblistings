23feff8609262f2acb5edcdb78453f4630bc819d
indaman1 <abubakar1133@gmail.com>
2025-07-18 14:17:33 -0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SessionController;
+use Illuminate\Support\Facades\Route;
+use App\Models\Job;
+use Illuminate\Support\Js;
+
+Route:: view('/', 'home');
+Route::view('/contact', 'contact');
+Route::resource('jobs', JobController::class);
+
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
+
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
+Route::post('/logout', [SessionController::class, 'destory']);
